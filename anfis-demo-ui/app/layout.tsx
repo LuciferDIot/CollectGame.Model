73994328f175d45f78692a7953b2ede9905f043a
Pasindu Geevinda <105873753+LuciferDIot@users.noreply.github.com>
2026-07-02 10:55:16 +0530

    chore: change defaultTheme to light in ThemeProvider

--- anfis-demo-ui/app/layout.tsx
+++ anfis-demo-ui/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`font-sans antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
         <Analytics />
